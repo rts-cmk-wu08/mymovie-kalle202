c79323ac37cf2e80b31d8627aed567e5e3de1913
Kjartan <kjartismarti@gmail.com>
2022-09-13 14:09:47 +0200

    added details js

--- nowshowing.js
+++ nowshowing.js
@@ -3,19 +3,24 @@ fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=5a42ea8b4fbbe41c
 .then(response => response.json())
 .then(data => {
     data.results.forEach(result => {
+
     let ratingcoversion = result.vote_average;
     let rating = Math.round(ratingcoversion * 10) / 10;
     const movieCard = document.createElement("article");
+
     movieCard.classList.add("movie__card");
     /* movieCard.classList.add("now__showing"); */
     movieCard.innerHTML = `
+
     <a href="details.html?id=${result.id}">
     <img class="poster__img" src="https://image.tmdb.org/t/p/w500${result.poster_path}">
     </a>
     <h3>${result.title}</h3>
     <div class="rating"><i class="fa-solid fa-star star"></i><p class="imdb__rating"><span>${rating}</span>/10 IMDb</p></div>
-    `
+    `;
+    
     nowShowing.append(movieCard);
+
     });
 });
 

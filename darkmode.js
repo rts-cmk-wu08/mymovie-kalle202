9d36d77ffdd82aa41271ca39728c8b313d80b9f8
Kjartan <kjartismarti@gmail.com>
2022-09-26 14:17:59 +0200

    initial darkmode functionality

---
+++ darkmode.js
@@ -0,0 +1,15 @@
+let setActiveStyleSheet = function (title) {
+let css = `link[rel="alternate stylesheet"]`;
+let stylesheets = document.querySelectorAll(css);
+console.log(stylesheets)
+}
+
+setActiveStyleSheet();
+
+
+/* const checkBox = document.querySelector('#checkbox');
+
+checkBox.addEventListener('change', ()=>{
+    document.body.classList.toggle('darkmode');
+
+}) */

1e08cd09309006608af62f3e700766bf5bf8e7a1
Kjartan <kjartismarti@gmail.com>
2022-09-26 08:11:12 +0200

    genres and popular added

--- details.js
+++ details.js
@@ -1,7 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
 
 let params = new URLSearchParams(window.location.search)
 let id = params.get("id")
-const movies = document.querySelector('#movies')
+const movies = document.querySelector('#movies')   
+const castList = document.querySelector(".cast__container") 
 
 
 fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=5a42ea8b4fbbe41ccef094fe698dbb64`)
@@ -9,11 +11,12 @@ fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=5a42ea8b4fbbe41ccef094fe
     .then(data => {
         let ratingcoversion = data.vote_average;
         let rating = Math.round(ratingcoversion * 10) / 10;
-        console.log(data)
+        
         let article = document.createElement('article')
         article.classList.add('movie__container')
         article.innerHTML = `
-        <img class="movie__backdrop" src="https://image.tmdb.org/t/p/w500${data.backdrop_path}" alt="Backdrop for ${data.title}">
+      
+
         <div class="info__container">
             <h2 class="movie__title">${data.title}<div class="rating"><i class="fa-solid fa-star star"></i><p class="imdb__rating"><span>${rating}</span>/10 IMDb</p></div></h2>
             
@@ -31,9 +34,9 @@ fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=5a42ea8b4fbbe41ccef094fe
             </div>
             <h3 class="movie__desc">Description</h3>
             <p class="movie__overview">${data.overview}</p>
-            <h3 class="cast__title">Cast</h3>
-            <button href="" class="master__button">See more</button>
-            <ul class="cast__container"></ul>
+            <div class="cast__title"><h3>Cast</h3>
+            <button href="" class="master__button">See more</button></div>
+            
             
         </div>
         `
@@ -44,9 +47,46 @@ fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=5a42ea8b4fbbe41ccef094fe
             let createGenre = document.createElement('li')
             createGenre.classList.add('genre')
             createGenre.innerHTML = `
-            <p>${genre.name}</p>
+            <p>${genre.name.toUpperCase()}</p>
             `;
             genreContainer.append(createGenre);
         });
 
     })
+
+    const trailer = document.querySelector('#trailer__container')
+
+fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=5a42ea8b4fbbe41ccef094fe698dbb64`)
+    .then(responseTwo => responseTwo.json())
+    .then(data => {
+        
+        let lastVideo = data.results.pop()
+        
+        let iframe = document.createElement('iframe')
+        iframe.setAttribute("src", `//www.youtube.com/embed/${lastVideo.key}`);
+        iframe.classList.add("trailer__iframe")
+        trailer.append(iframe);
+    })
+
+
+console.log(castList)
+
+fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=5a42ea8b4fbbe41ccef094fe698dbb64`)
+.then(response => response.json())
+.then(data => {
+    data.cast.forEach((actor,index) => {
+        let castListItem = document.createElement('li')
+        castListItem.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500/${actor.profile_path}">
+        <h5>${actor.name}</h5>
+        `
+        if(index<4){castList.append(castListItem)
+        }
+        
+        
+       
+    })
+    
+})
+
+})
--- popular.js
+++ popular.js
@@ -1,18 +1,47 @@
-let nowShowing = document.querySelector(".popular");
-fetch("https://api.themoviedb.org/3/trending/movie/week?api_key=5a42ea8b4fbbe41ccef094fe698dbb64")
+let popular = document.querySelector(".popular");
+fetch("https://api.themoviedb.org/3/movie/popular?api_key=5a42ea8b4fbbe41ccef094fe698dbb64")
 .then(response => response.json())
 .then(data => {
     data.results.forEach(result => {
-    let ratingcoversion = result.vote_average;
-    let rating = Math.round(ratingcoversion * 10) / 10;
-    const movieCard = document.createElement("article");
-    movieCard.classList.add("movie__card");
-    /* movieCard.classList.add("now__showing"); */
-    movieCard.innerHTML = `
-    <img class="poster__img" src="https://image.tmdb.org/t/p/w500${result.poster_path}">
-    <h3>${result.title}</h3>
-    <div class="rating"><i class="fa-solid fa-star star"></i><p class="imdb__rating"><span>${rating}</span>/10 IMDb</p></div>
-    `
-    nowShowing.append(movieCard);
-    });
+        let createDiv = document.createElement('div')
+
+        let ratingCoversion = result.vote_average;
+        let rating = Math.round(ratingCoversion * 10) / 10;
+
+        createDiv.classList.add('popular__movie')
+        createDiv.innerHTML = `
+        <a href="details.html?id=${result.id}">
+        <img class="pop__poster" src="https://image.tmdb.org/t/p/w500/${result.poster_path}">
+        </a>
+        <div class="pop__info">
+        <h3>${result.title}</h3>
+        <div class="rating"><i class="fa-solid fa-star star"></i><p class="imdb__rating"><span>${rating}</span>/10 IMDb</p></div>
+        <ul class="genre__container"></ul>
+       
+        </div>
+        `;
+        popular.append(createDiv)
+
+       
+        const popGenres = createDiv.querySelector(".genre__container")
+        result.genre_ids.forEach(id => {
+            
+            let currentGenre = genres.find(genre => genre.id == id)
+            let makeGenre = document.createElement('li')
+            makeGenre.classList.add('genre')
+            makeGenre.innerHTML = `
+            <p>${currentGenre.name.toUpperCase()}</p>
+            `
+            popGenres.append(makeGenre)
+        })
+  /*       const genreContainer = document.querySelector('.genre__container')
+        data.genres.forEach(genre => {
+            let createGenre = document.createElement('li')
+            createGenre.classList.add('genre')
+            createGenre.innerHTML = `
+            <p>${genre.name.toUpperCase()}</p>
+            `;
+            genreContainer.append(createGenre);
+        }); */
+    })
 })
